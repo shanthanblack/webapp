42c5a8601f794c9f6d4181f00c2ab5be824a4587
shanthanblack <43510477+shanthanblack@users.noreply.github.com>
2020-06-16 13:25:23 +0100

    Update port.sh

--- port.sh
+++ port.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 nmap -sV 13.232.120.52 -A -v
 nmap -p 445 -script=smb-vuln-ms17-010.nse 13.232.120.152
-nmap --script smb-vuln-ms17-010.nse -p 445 13.232.120.152 --script-args=unsafe=1
 
 
